7efaf8c5cf8a3d257fb3f1e14fc853cc3d144787
Jay M <jay@suitedconnector.com>
2025-02-21 15:32:01 -0700

    one more

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './environment',
+  envDir: "./environment",
   plugins: [react()],
   server: {
     port: 3000,
-    host: '0.0.0.0'
+    host: "0.0.0.0",
+    allowedHosts: [
+      "candidate-search-application-with-reactjs-0v9s.onrender.com",
+    ],
   },
   base: "/",
-  preview: {
-    host: true,
-    port: 3000,
-    strictPort: true,
-    allowedHosts: ['candidate-search-application-with-reactjs-0v9s.onrender.com']
-  }
 });
